0ddb44a4c2fbd50112a03a121741a7ff2e221b50
Hiago Menezes <hiago@better.corp>
2019-11-11 15:39:33 -0300

    change

---
+++ TelaRPS/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace TelaRPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.ListaNotas = new System.Windows.Forms.DataGridView();
+            this.Column6 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnlinha = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ListaNotas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(524, 457);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "GERAR LOTE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(661, 374);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ativos";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(661, 397);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(81, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Cancelados";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(661, 420);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Todos";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // ListaNotas
+            // 
+            this.ListaNotas.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListaNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListaNotas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column6,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.ListaNotas.Location = new System.Drawing.Point(3, 26);
+            this.ListaNotas.Name = "ListaNotas";
+            this.ListaNotas.RowHeadersWidth = 145;
+            this.ListaNotas.RowTemplate.Height = 20;
+            this.ListaNotas.Size = new System.Drawing.Size(649, 425);
+            this.ListaNotas.TabIndex = 6;
+            this.ListaNotas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ListaNotas_CellContentClick);
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Column6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Column6.Frozen = true;
+            this.Column6.HeaderText = "Selecionar";
+            this.Column6.Name = "Column6";
+            this.Column6.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column6.Width = 63;
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "Nome";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.Frozen = true;
+            this.Column3.HeaderText = "Data";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.Frozen = true;
+            this.Column4.HeaderText = "Valor";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(661, 358);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Filtro";
+            // 
+            // btnlinha
+            // 
+            this.btnlinha.Location = new System.Drawing.Point(403, 457);
+            this.btnlinha.Name = "btnlinha";
+            this.btnlinha.Size = new System.Drawing.Size(115, 40);
+            this.btnlinha.TabIndex = 8;
+            this.btnlinha.Text = "Quantidade de linhas selecionadas";
+            this.btnlinha.UseVisualStyleBackColor = true;
+            this.btnlinha.Click += new System.EventHandler(this.btnlinha_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Selecionar tudo";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 501);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnlinha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListaNotas);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "TelaDeGeraçãoDeLoteRPS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ListaNotas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.DataGridView ListaNotas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button btnlinha;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ TelaRPS/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TelaRPS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ListaNotas.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            StreamWriter MeuCaminho = null;
+
+            SaveFileDialog GerarLote = new SaveFileDialog();
+            //Escolher onde salvar o arquivo
+            //GerarLote.InitialDirectory = @"C:\Users\hiago\Desktop\teste";
+            GerarLote.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            // metodo para filtra o tipo do arquivo que vai ser salvo
+            GerarLote.Filter = "arquivos txt (*.txt)|*.txt";
+            // codigo para retornar para o dioretorio quando salvar o arquivo
+            // GerarLote.RestoreDirectory = true;
+            // obtém ou define o título da caixa de diálogo do arquivo
+            GerarLote.Title = "Lote RPS";
+
+
+            // contar todas as colunas do datagridview
+            int qtdColunas = ListaNotas.Columns.Count;
+
+            if (GerarLote.ShowDialog() == DialogResult.OK)
+            {
+                //Pega o caminho do arquivo
+                string caminho = GerarLote.FileName;
+                //Cria um StreamWriter no local
+                MeuCaminho = new StreamWriter(caminho);
+
+                foreach (DataGridViewRow item in ListaNotas.Rows)
+                {
+                    if (Convert.ToBoolean(item.Cells[0].Value) == true)
+                    {
+                        string linha = null;
+                        for (int i = 0; i < qtdColunas; i++)
+                        {
+                            linha += item.Cells[i].Value.ToString();
+                        }
+                        MeuCaminho.WriteLine(linha);
+                    }
+                }
+                MeuCaminho.Close();
+                MessageBox.Show("Salvo com sucesso!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            string[,] Linhas = new string[,]{
+             {"false","1","Hiago","2019-10-31","10"},
+             {"false","2","Wellington","2019-09-28","20"},
+             {"false","3","Denis","2019-08-26","30"},
+             {"false","4","Vanessa","2019-07-10","40"},
+             {"false","5","JWill","2019-06-15","50"},
+             {"false","6","Carlor","2019-05-20","60"},
+             {"false","7","Roberto","2019-04-01","70"}
+            };
+
+            // Linhas.GetLength(0)comprimento de retorno do primeiro D (7)
+            // Linhas.GetLength(1) comprimento de retorno do segundo D
+            for (int i = 0; i < Linhas.GetLength(0); i++)
+            {
+                string[] Linha = new string[Linhas.GetLength(1)];
+
+                for (int j = 0; j < Linhas.GetLength(1); j++)
+                {
+                    Linha[j] = Linhas[i, j];
+                }
+
+
+                ListaNotas.Rows.Add(Linha);
+                // função para não permiter que coloque a ultima linha 
+                ListaNotas.AllowUserToAddRows = false;
+
+
+                // Não permite que o usuario fazer alteração na tabela
+                //ListaNotas.ReadOnly = true;
+
+            }
+
+        }
+
+        private void ListaNotas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            //var selecionar = (DataGridView)sender;
+            //selecionar.EndEdit();
+
+            //foreach (DataGridViewRow row in ListaNotas.Rows)
+            //{
+            //    if (e.RowIndex >= 0)
+            //    {
+            //        var cbxCell = (DataGridViewCheckBoxCell)selecionar.Rows[e.RowIndex].Cells[0];
+            //        if ((bool)cbxCell.Value)
+            //       // if (Convert.ToBoolean(cbxCell.Value))
+            //        {
+            //            button2.Enabled = true;
+            //        }
+            //        else
+            //        {
+            //            button2.Enabled = false;
+            //        }
+            //    }
+            //}
+
+            if (e.ColumnIndex == 0 && e.RowIndex > -1)
+            {
+                ListaNotas.CommitEdit(DataGridViewDataErrorContexts.Commit);
+                var i = 0;
+                foreach (DataGridViewRow row in ListaNotas.Rows)
+                {
+                    if (Convert.ToBoolean(row.Cells[0].Value))
+                    {
+                        i++;
+                    }
+                }
+
+
+                if (i > 0)
+                {
+                    button2.Enabled = true;
+                }
+                else
+                {
+                    button2.Enabled = false;
+                }
+            }
+
+        }
+        private void btnlinha_Click(object sender, EventArgs e)
+        {
+
+            int Totallinhas = Convert.ToInt32(ListaNotas.Rows.Count.ToString());
+            int selecionados = 0;
+            for (int x = 0; x < Totallinhas; x++)
+            {
+                if (ListaNotas.Rows[x].Cells[0].Value.ToString() == "True")
+                {
+                    selecionados++;
+                }
+            }
+            if (selecionados == 0) MessageBox.Show("Nenhum linha foi selecionada", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            if (selecionados > 0) MessageBox.Show(selecionados + " linha(s) selecionada(s)", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in ListaNotas.Rows)
+            {
+                item.Cells[0].Value = (sender as CheckBox).Checked;
+            }
+
+            if (checkBox1.Checked == true)
+            {
+                button2.Enabled = true;
+            }
+            else
+            {
+                button2.Enabled = false;
+            }
+        }
+
+    }
+
+}
